ff137888aa6404b2072db053507286e4fc62938c
Joey Nichols <nichols@bermanco.com>
2015-07-02 17:53:45 -0400

    Removed call to method on the wp_post_obj on line 54

--- StandardPost.php
+++ StandardPost.php
@@ -51,7 +51,7 @@ class StandardPost {
 	 */
 	public function get_excerpt($length_in_words = 30, $suffix = '&hellip;'){
 
-		if ($this->wp_post_obj->post_excerpt()){
+		if ($this->wp_post_obj->post_excerpt){
 			return apply_filters('the_excerpt', $this->$this->wp_post_obj->post_excerpt());
 		}
 		else if ($this->get_content()){
