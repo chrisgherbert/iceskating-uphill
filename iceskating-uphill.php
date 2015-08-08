a0bad7139b44847850d27797db06613d888123a2
Chris Herbert <herbert@bermanco.com>
2015-08-08 19:32:11 -0400

    Changed method of loading class files - should be a little more compact

--- iceskating-uphill.php
+++ iceskating-uphill.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Ice Skating Uphill - Object Oriented Posts
  * Plugin URI:        https://github.com/chrisgherbert/iceskating-uphill/
  * Description:       Provides classes to work with WordPress posts and taxonomy terms in simpler, more object-oriented way
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Chris Herbert
  * Author URI:        http://chrisgherbert.com/
  * License:           GPL-2.0+
@@ -16,20 +16,25 @@ if (!defined('WPINC')){
 	die;
 }
 
-$plugin_path = plugin_dir_path(__FILE__);
+$iceskating_uphill_plugin_path = plugin_dir_path(__FILE__);
 
-if (!class_exists('StandardPost')){
-	require_once($plugin_path . 'StandardPost.php');
+$iceskating_uphill_classes = array(
+	'StandardPost',
+	'StandardTaxonomyTerm',
+	'StandardQueries',
+	'StandardUser'
+);
+
+foreach ($iceskating_uphill_classes as $class){
+
+	$full_path = $iceskating_uphill_plugin_path . $class . '.php';
+
+	if (!class_exists($class) && file_exists($full_path)){
+		require_once($full_path);
+	}
+
 }
 
-if (!class_exists('StandardTaxonomyTerm')){
-	require_once($plugin_path . 'StandardTaxonomyTerm.php');
-}
-
-if (!class_exists('StandardQueries')){
-	require_once($plugin_path . 'StandardQueries.php');
-}
-
-add_action('after_setup_theme',function(){
+add_action('after_setup_theme', function(){
 	do_action('iceskating_uphill_loaded');
 });
